d51bc134538a7a26ceacc3289ab81a0d6dd67f00
Mohamed Azharudeen <125290366+azharlabs@users.noreply.github.com>
2025-12-06 06:13:13 +0530

    chore: remove setupGithubCommand

--- packages/cli/src/services/BuiltinCommandLoader.ts
+++ packages/cli/src/services/BuiltinCommandLoader.ts
@@ -37,7 +37,6 @@ import { terminalSetupCommand } from '../ui/commands/terminalSetupCommand.js';
 import { themeCommand } from '../ui/commands/themeCommand.js';
 import { toolsCommand } from '../ui/commands/toolsCommand.js';
 import { vimCommand } from '../ui/commands/vimCommand.js';
-import { setupGithubCommand } from '../ui/commands/setupGithubCommand.js';
 
 /**
  * Loads the core, hard-coded slash commands that are an integral part
@@ -85,7 +84,6 @@ export class BuiltinCommandLoader implements ICommandLoader {
       toolsCommand,
       settingsCommand,
       vimCommand,
-      setupGithubCommand,
       terminalSetupCommand,
     ];
 
